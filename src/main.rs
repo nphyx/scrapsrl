dc9158b17917eb1147860cb6e08eb3530274b1d9
Justen Robertson <justen@justen.us>
2019-02-01 06:01:26 -0800

    feat: display module to handle display drawing

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 extern crate rand;
 extern crate tcod;
 
-use tcod::{Console, RootConsole, FontLayout, FontType, BackgroundFlag};
-use tcod::colors::{Color, lerp};
 use tcod::input::Key;
 use tcod::input::KeyCode::{F11, Escape};
 use tcod::map::FovAlgorithm;
@@ -15,17 +13,15 @@ mod constants;
 mod entity;
 mod util;
 mod player;
-use crate::util::{clamp, plan};
+mod display;
+use crate::display::Display;
+use crate::util::plan;
 use crate::entity::{Coord, Entity, Character};
 use crate::game_state::GameState;
 use crate::constants::{
     TORCH_RADIUS,
     MAP_WIDTH,
-    MAP_HEIGHT,
-    SCREEN_WIDTH,
-    SCREEN_HEIGHT,
-    DEFAULT_BG,
-    DEFAULT_FG};
+    MAP_HEIGHT};
 
 
 fn move_bug(&pos: &Coord, map: &tcod::map::Map) -> Option<Coord> {
@@ -37,23 +33,8 @@ fn move_bug(&pos: &Coord, map: &tcod::map::Map) -> Option<Coord> {
     plan(&to, &map)
 }
 
-fn distance(px: f32, py: f32, dx: f32, dy: f32) -> f32 {
-    return ((dx - px).powf(2.0) + (dy - py).powf(2.0)).sqrt()
-}
-
 fn main() {
-    let mut root = RootConsole::initializer()
-        .font("monofur-nf.png", FontLayout::AsciiInRow)
-        .font_type(FontType::Greyscale)
-        .font_dimensions(102,636)
-        .size(SCREEN_WIDTH, SCREEN_HEIGHT)
-        .title("SCRAPS: Bug Hunter")
-        .init();
-
-    root.set_default_background(DEFAULT_BG);
-    root.set_default_foreground(DEFAULT_FG);
-
-    
+    let mut display = Display::new();
     let cx = MAP_WIDTH / 2;
     let cy = MAP_HEIGHT / 2;
     let mut rng = rand::thread_rng();
@@ -64,7 +45,6 @@ fn main() {
         x: rng.gen_range(0, MAP_WIDTH),
         y: rng.gen_range(0, MAP_HEIGHT)
     });
-    root.clear();
 
     let (map, tiles) = mapgen::generate(MAP_WIDTH, MAP_HEIGHT);
     let mut state = GameState::new(Character::blank(), map, tiles);
@@ -74,10 +54,6 @@ fn main() {
     // Use a max_radius of 10 and light the walls.
     state.map.compute_fov(20,20, TORCH_RADIUS, true, FovAlgorithm::Basic);
 
-    let light = Color::new(200, 180, 50);
-    let dark = Color::new(0, 6, 18);
-    let ground = DEFAULT_BG; //Color::new(0, 40, 25);
-
     interface.open_menu(
         ui::Notification::new(
             format!("Start Game"),
@@ -85,41 +61,8 @@ fn main() {
             )
     );
 
-    while !root.window_closed() {
-        root.clear();
-        // Compute the FOV starting from the coordinates 20,20. Where we'll put the '@'
-        // Use a max_radius of 10 and light the walls.
-        state.map.compute_fov(state.player.pos().x, state.player.pos().y, 15, true, FovAlgorithm::Basic);
-
-        for ((px, py), tile) in &state.tiles {
-            let visible = state.map.is_in_fov(*px, *py);
-            let dist = clamp(
-                0.0,
-                1.0,
-                distance(state.player.pos().x as f32, state.player.pos().y as f32, *px as f32, *py as f32)
-                / TORCH_RADIUS as f32);
-            let fg: Color;
-            let bg: Color;
-            let blend = lerp(light, dark, dist);
-            if visible && (dist < TORCH_RADIUS as f32) {
-                bg = lerp(ground, blend, 0.3);
-                fg = lerp(tile.color, blend, 0.7);
-            } else if visible {
-                bg = lerp(ground, blend, 0.5);
-                fg = lerp(tile.color, blend, 0.5);
-            } else {
-                bg = lerp(ground, dark, 0.5);
-                fg = lerp(tile.color, dark, 0.7);
-            }
-            root.put_char_ex(*px, *py, tile.ch, fg, bg);
-        }
-
-        if state.map.is_in_fov(bug.pos().x, bug.pos().y) {
-            root.put_char(bug.pos().x, bug.pos().y, '\u{f46f}', BackgroundFlag::None);
-            root.set_char_foreground(bug.pos().x, bug.pos().y, Color{r: 32, g: 128, b: 225});
-        }
-        root.put_char(state.player.pos().x, state.player.pos().y, '\u{e213}', BackgroundFlag::None);
-
+    while !display.root.window_closed() {
+        // game success state
         if state.player.pos() == bug.pos() {
             state.score += 1;
             bug.set_pos(Coord{
@@ -132,11 +75,12 @@ fn main() {
                     format!("Success"),
                     format!("Got 'em!"),
                     )
-                );
+            );
         }
-        interface.draw(&root, &state);
-        root.flush();
-        let keypress = root.wait_for_keypress(true);
+
+        state.map.compute_fov(state.player.pos().x, state.player.pos().y, 15, true, FovAlgorithm::Basic);
+        display.draw(&state, &bug, &mut interface);
+        let keypress = display.root.wait_for_keypress(true);
         // libtcod 1.5.1 has a bug where `wait_for_keypress` emits two events:
         // one for key down and one for key up. So we ignore the "key up" ones.
         if keypress.pressed {
@@ -144,24 +88,21 @@ fn main() {
             match keypress {
                 Key { code: F11, .. } => {
                     fullscreen = !fullscreen;
-                    root.set_fullscreen(fullscreen);
+                    display.root.set_fullscreen(fullscreen);
                 },
                 _ => {}
             }
             if !interface.handle_input(keypress, &mut state) {
                 if state.player.handle_input(&keypress, &state.map) {
-                    println!("player acted");
                     match move_bug(&bug.pos(), &state.map) {
                         Some(coord) => bug.set_pos(coord),
                         _ => {}
                     }
                     state.player.tick();
-                } else {
-                    match keypress {
-                        Key { code: Escape, .. } => break,
-                        _ => {}
-                    }
-
+                }
+                match keypress {
+                    Key { code: Escape, .. } => break,
+                    _ => {}
                 }
             }
         }
--- src/util.rs
+++ src/util.rs
@@ -6,6 +6,10 @@ pub fn clamp<T>(a: T, b: T, x: T) -> T  where T: std::cmp::PartialOrd {
     if x < a { a } else if x > b { b } else { x }
 }
 
+pub fn distance(px: f32, py: f32, dx: f32, dy: f32) -> f32 {
+    return ((dx - px).powf(2.0) + (dy - py).powf(2.0)).sqrt()
+}
+
 pub fn plan(&to: &Coord, map: &tcod::map::Map) -> Option<Coord> {
     let planned = Coord{
         x: clamp(0, MAP_WIDTH - 1, to.x),
